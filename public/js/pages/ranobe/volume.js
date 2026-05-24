8a678c2833482bb58a64317695b4f6230d80b090
MrEgor4ik <mregor4ikgaming@gmail.com>
2026-05-24 23:30:16 +0000

    feat: click on sides in fucking slider

--- public/js/pages/ranobe/volume.js
+++ public/js/pages/ranobe/volume.js
@@ -7,17 +7,34 @@ document.addEventListener('DOMContentLoaded', () => {
       skipSnaps: false,
     })
 
-    const slides = root.querySelectorAll('.embla__slide')
+    const slides = Array.from(root.querySelectorAll('.embla__slide'))
 
     function updateSelected() {
       const selected = embla.selectedScrollSnap()
 
       slides.forEach((slide, index) => {
-        slide.classList.toggle('is-selected', index === selected)
+        const isSelected = index === selected
+
+        slide.classList.toggle('is-selected', isSelected)
+
+        const image = slide.querySelector('.carousel-img')
+        if (image) {
+          image.classList.toggle('is-clickable', !isSelected)
+        }
       })
     }
 
-    embla.on('init', updateSelected)
+    slides.forEach((slide, index) => {
+      const image = slide.querySelector('.carousel-img')
+      if (!image) return
+
+      image.addEventListener('click', () => {
+        if (index === embla.selectedScrollSnap()) return
+
+        embla.scrollTo(index)
+      })
+    })
+
     embla.on('select', updateSelected)
     embla.on('reInit', updateSelected)
 
