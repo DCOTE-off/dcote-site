5b0d09585fe55e5058d096a78b358ee33ba55bad
andreev06 <aandreev4884@gmail.com>
2026-06-09 17:24:47 +0200

    feat(admin): allow setting update feed date

--- app/Filament/Admin/Resources/UpdateFeedResource/Pages/CreateUpdateFeed.php
+++ app/Filament/Admin/Resources/UpdateFeedResource/Pages/CreateUpdateFeed.php
@@ -9,4 +9,13 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUpdateFeed extends CreateRecord
 {
     protected static string $resource = UpdateFeedResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (empty($data['created_at'])) {
+            $data['created_at'] = now();
+        }
+
+        return $data;
+    }
 }
