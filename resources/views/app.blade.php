c24908777c0f6cffa3d9d9631fcf8eb00b0bf023
MrEgor4ik <mregor4ikgaming@gmail.com>
2026-09-17 23:28:52 +0300

    feat(metrics): load presence tracker from the Inertia root view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -115,5 +115,26 @@
 <body>
     @include('partials.svg_icons')
     @inertia
+
+    @php
+        $metricsBaseUrl = (string) config('services.dcote.metrics_base_url');
+    @endphp
+    <script>
+        window.DCOTE_SITE_METRICS = {
+            contractVersion: 1,
+            metricsBaseUrl: @json($metricsBaseUrl),
+            userId: @json(
+                auth()->check()
+                    ? hash_hmac('sha256', (string) auth()->id(), (string) config('app.key'))
+                    : null
+            ),
+            page: @json(request()->route()?->getName() ?? request()->route()?->uri() ?? request()->path()),
+        };
+    </script>
+    <script
+        defer
+        data-dcote-site-presence
+        src="{{ $metricsBaseUrl }}/site-presence-tracker.js"
+    ></script>
 </body>
 </html>
