2f549cbca64573644f7b8ed67b154a276829262d
MrEgor4ik <mregor4ikgaming@gmail.com>
2026-09-19 22:34:50 +0300

    feat(deploy): add a scheduler container

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,12 +15,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sitemap:generate')
-            ->dailyAt('03:00')
-            ->sendOutputTo(storage_path('logs/sitemap.log'));
+                ->dailyAt('03:00')
+                ->timezone('Europe/Moscow')
+                ->sendOutputTo(storage_path('logs/sitemap.log'));
 
         $schedule->command('publication:sync')
-            ->everyMinute()
-            ->withoutOverlapping();
+                ->everyMinute()
+                ->withoutOverlapping();
     }
 
     /**
